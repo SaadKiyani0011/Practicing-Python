baa6ddede002cf50e590f08bbc01a4c4076115f6
Saad Zia Kiyani <saadkiyanioffical@gmail.com>
2025-08-04 11:41:38 -0700

    Operators

--- tutt1.py
+++ tutt1.py
@@ -1,2 +1,8 @@
 a = float(input("enter a number"))
 print("entered",a,"f off")
+
+int # 1,3,12333
+float #1.0,1.9854
+str # "hello", "1234"
+complex # 1+2j, 3+4j
+bool # True, False
---
+++ tutt2.py
@@ -0,0 +1,12 @@
+# Operators 
+
+a = 90
+b = 20
+c = a+b #adition
+d = a-b #subtraction
+e = a*b #multiplication
+f = a/b #division
+g = a%b #modulus
+h = a**b #power
+i = a//b # Floor Division operator.
+print (i)
