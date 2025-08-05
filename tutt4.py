15a016a6a1a74e77baa345e8bb8cfe1750170d1f
Saad Zia Kiyani <saadkiyanioffical@gmail.com>
2025-08-05 10:57:14 -0700

    Logical Operators:

---
+++ tutt4.py
@@ -0,0 +1,7 @@
+# Logical Operators:
+a = 10
+b = 20
+c = 30
+print(a < b and b < c)  # True if both conditions are true
+print(a < b or b > c)   # True if at least one condition is true
+print(not(a < b))       # True if the condition is false
