5ce27bb2e934e31e446b38ed3fdea531a228c919
Saad Zia Kiyani <saadkiyanioffical@gmail.com>
2025-08-05 11:33:43 -0700

    If else

---
+++ tutt6.py
@@ -0,0 +1,15 @@
+#If else Stements
+a = 5
+b = 2
+
+# if a and b > 10:
+#     print("a is less than b")
+# elif a == b:
+#     print("a is equal b")
+# else:
+#     print("a is not less than b")
+
+if a%2==0:
+    print("a is even")
+else:
+    print("a is odd")
