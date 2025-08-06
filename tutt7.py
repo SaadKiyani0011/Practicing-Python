86c584a4a2d797518dc1005a3f45902a5ea7ea74
Saad Zia Kiyani <saadkiyanioffical@gmail.com>
2025-08-06 11:13:33 -0700

    LOops

--- tutt7.py
+++ tutt7.py
@@ -1,7 +1,19 @@
 #Loops in Python 
 # for a in range(2,50):
 #     print("Girand Operation")
-a = 0
-while a < 10:
-    print("While Loop Operation")
+
+
+# a = 0
+# while a < 10:
+#     print("While Loop Operation")
+#     a += 1
+    
+# for a in range(1, 10):
+#     if a % 2 == 0:
+#         print(a)
+
+a = 1
+while a <= 10:
+    if a % 2 == 0:
+        print(a)
     a += 1
