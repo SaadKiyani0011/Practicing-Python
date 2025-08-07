3acf47f438ce97c1058c2124fb4b96e42f39488b
Saad Zia Kiyani <saadkiyanioffical@gmail.com>
2025-08-07 11:20:04 -0700

    Lists

--- tutt7.py
+++ tutt7.py
@@ -12,8 +12,21 @@
 #     if a % 2 == 0:
 #         print(a)
 
-a = 1
-while a <= 10:
-    if a % 2 == 0:
-        print(a)
-    a += 1
+# a = 1
+# while a <= 10:
+#     if a % 2 == 0:
+#         print(a)
+#     a += 1
+
+# for a in range(1, 10):
+#     if a==5:
+#         continue
+#     print(a)
+
+password = input("Enter your password: ")
+
+while password != "secret123":
+    print("Incorrect password, try again.")
+    password = input("Enter your password: ")
+
+print("Access granted")
---
+++ tutt8.py
@@ -0,0 +1,4 @@
+a = ["Aadil","Sarah","Aamir","Aisha","Ali","Sana","Ahmed","Sadia"]
+b = ["amir",10,True,20.5]
+c = [1,2,4,5]
+print(type(c))
