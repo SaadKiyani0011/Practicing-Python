3d99f44e78a446e6c7f34f6ad2de5f2775e0b679
Saad Zia Kiyani <saadkiyanioffical@gmail.com>
2025-08-07 11:41:24 -0700

    lists

---
+++ tutt10.py
@@ -0,0 +1,11 @@
+a = ["Aadil", "Aamir" , "Ali", "Sarah", "Ayesha", "Qazi", "Ahmed", "Sofia"]
+
+# a[4] = "Bharwa"
+# print(a)  
+
+# a[1:5] = ["Salman", "Sana", "Sadia", "Aamir", "Aisha"] 
+
+# print(a)
+
+a.insert(2, "10")
+print(a)
--- tutt9.py
+++ tutt9.py
@@ -1,2 +1,9 @@
-a = ["harry", "sarah", "aamir", "aisha", "ali", "sana", "ahmed", "sadia"]
-print(a[-2])  # Accessing the first element
+a = ["harry", "sarah", "Aamir", "aisha", "ali", "sana", "ahmed", "sadia"]
+# print(a[-2])  # Accessing the first element
+
+# print(a[1:2])
+
+if "Aamir" in a:
+    print("Yes, Aamir is in the list")
+else:
+    print("No, Aamir is not in the list")
