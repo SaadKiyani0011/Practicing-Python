43c6064ba5966b7fa6a35d3c65973d503bc2bd2d
Saad Zia Kiyani <saadkiyanioffical@gmail.com>
2025-08-07 12:10:44 -0700

    lists

---
+++ tutt13.py
@@ -0,0 +1,14 @@
+# a = ["Ali","Ahmed","Hasham","Aadil","Nono","Saad"]
+# for z in a:
+#     print(z)
+    
+    
+# thislist = ["apple", "banana", "cherry"]
+# i = 0
+# while i < len(thislist):
+#   print(thislist[i])
+#   i = i + 1
+
+
+thislist = ["apple", "banana", "cherry"]
+[print(x) for x in thislist]
